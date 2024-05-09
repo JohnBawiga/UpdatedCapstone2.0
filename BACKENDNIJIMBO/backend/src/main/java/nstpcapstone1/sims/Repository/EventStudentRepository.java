b8d6f011979a02b35903e5fb653d19df9cd4a252
JohnBawiga <jmjay800@gmail.com>
2024-05-09 22:50:47 +0800

    Updated

---
+++ BACKENDNIJIMBO/backend/src/main/java/nstpcapstone1/sims/Repository/EventStudentRepository.java
@@ -0,0 +1,20 @@
+package nstpcapstone1.sims.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nstpcapstone1.sims.Entity.EventStudentEntity;
+import nstpcapstone1.sims.Entity.EventTeacherEntity;
+
+import java.util.List;
+
+public interface EventStudentRepository extends JpaRepository<EventStudentEntity, Long> {
+    List<EventStudentEntity> findByEventEventID(Long eventId);
+    List<EventStudentEntity> findByStudentUserid(Long userid);
+    Optional<EventStudentEntity> findByEventEventIDAndStudentUserid(Long eventId, Long studentId);
+    List<EventStudentEntity> findByStudentUseridAndEventEventID(Long userid, Long eventID);
+
+
+}
